2d4152d779411f233b33ab0b2a3cf56e2677f96d
Washington Awala <washingtonawala@washingtonawala@webmasters.co.ke>
2022-02-01 19:32:10 +0300

    laravel vue

---
+++ resources/js/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <div><h1> HELLO VUE 3</h1></div>
+</template>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel - Vue3</title>
+
+        <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+
+    </head>
+    <body class="antialiased">
+       <div id="app">
+
+       </div>
+       <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
